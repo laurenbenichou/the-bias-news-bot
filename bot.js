422d03bede6ae5f6b6befa4b6d39bebc2d804eff
Lo Benichou <laurenbenichou@gmail.com>
2016-11-17 22:33:17 -0800

    prep for heroku

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 var Twit = require( 'twit' );
 var DotEnv = require('dotenv').config();
+var prettyjson = require('prettyjson');
 
 var T = new Twit({
 	consumer_key:         process.env.TWIT_KEY,
@@ -8,3 +9,24 @@ var T = new Twit({
 	access_token_secret:  process.env.TWIT_ACCESS_TOKEN_SECRET,
 	timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 })
+
+var options = { track: ['rawstory com', 'infowars com', 'breitbart com', 'prisonplanet com'] };
+var domains = ['rawstory', 'infowars', 'breitbart', 'prisonplanet'];
+var regexDomains = new RegExp( domains.join('|') );
+var stream = T.stream( 'statuses/filter', options);
+
+stream.on( 'tweet', function( tweet ) {
+	if ( tweet.entities.urls[0] !== undefined ) {
+		if ( tweet.entities.urls[0].expanded_url !== null ) {
+			if ( regexDomains.test( tweet.entities.urls[0].expanded_url ) ){
+				console.log( tweet.entities.urls[0].expanded_url );
+			}
+		}
+
+	}
+
+});
+
+stream.on( 'error', function( error ) {
+	console.log( error );
+});
